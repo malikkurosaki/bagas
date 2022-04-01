765c1e222986f2e78518a6b9a2e28151cc5e9659
malikkurosaki <kurosakiblackangel@gmail.com>
2022-04-01 22:01:24 +0800

    ya

---
+++ xbin/_color.sh
@@ -0,0 +1,18 @@
+_red(){
+   echo "$(tput setaf 1)$1$(tput sgr0)"
+}
+_green(){
+   echo "$(tput setaf 2)$1$(tput sgr0)"
+}
+_yellow(){
+   echo "$(tput setaf 3)$1$(tput sgr0)"
+}
+_blue(){
+   echo "$(tput setaf 4)$1$(tput sgr0)"
+}
+_purple(){
+   echo "$(tput setaf 5)$1$(tput sgr0)"
+}
+_cyan(){
+   echo "$(tput setaf 6)$1$(tput sgr0)"
+}
--- xbin/_menu.sh
+++ xbin/_menu.sh
@@ -1,3 +1,15 @@
-_menu(){
-    echo "ini menu"
+_menu() {
+    _green "
+    Menu:
+
+    "
+    _cyan "Pilih Menu"
+    read -p ": " menu
+    while true; do
+        case $menu in
+        *)
+            _red "menu belum ada"
+            ;;
+        esac
+    done
 }
