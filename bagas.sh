672bba7f2c7be3c4bf6694f0f762e6d601ce05a1
malikkurosaki <kurosakiblackangel@gmail.com>
2022-04-01 21:09:28 +0800

    dua

--- bagas.sh
+++ bagas.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-. <(curl -s "https://raw.githubusercontent.com/malikkurosaki/bagas/main/bagas.sh")
+. <(curl -s "https://raw.githubusercontent.com/malikkurosaki/bagas/main/xbin/autoload.sh")
 _autoload
