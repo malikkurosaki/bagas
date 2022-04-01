3d2a7d439970e781f2f8f9369c07404f8abc719d
malikkurosaki <kurosakiblackangel@gmail.com>
2022-04-01 22:39:15 +0800

    j

--- xbin/_git.sh
+++ xbin/_git.sh
@@ -18,21 +18,19 @@ _git() {
     _yellow "q | Quit | CTRL+C untuk keluar"
     _cyan "Pilih Menu Git"
     read -p ": " menu
-    while true; do
-        case $menu in
-        p | push)
-            git add
-            git commit -m "$(date)"
-            git push
-            exit 0
-            ;;
-        q | quit)
-            _cyan "bye ..."
-            exit 0
-            ;;
-        *)
-            _red "menu belum ada"
-            ;;
-        esac
-    done
+    case $menu in
+    p | push)
+        git add
+        git commit -m "$(date)"
+        git push
+        exit 0
+        ;;
+    q | quit)
+        _cyan "bye ..."
+        exit 0
+        ;;
+    *)
+        _red "menu belum ada"
+        ;;
+    esac
 }
--- xbin/_menu.sh
+++ xbin/_menu.sh
@@ -6,18 +6,16 @@ _menu() {
     _yellow "q | Quit | CTRL+C untuk keluar"
     _cyan "Pilih Menu"
     read -p ": " menu
-    while true; do
-        case $menu in
-        g | git)
-            _git
-            ;;
-        q | quit)
-            _cyan "bye ..."
-            exit 0
-            ;;
-        *)
-            _red "menu belum ada"
-            ;;
-        esac
-    done
+    case $menu in
+    g | git)
+        _git
+        ;;
+    q | quit)
+        _cyan "bye ..."
+        exit 0
+        ;;
+    *)
+        _red "menu belum ada"
+        ;;
+    esac
 }
