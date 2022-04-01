8a4143a23e11bd2d6faa004d4e5e4f361cdca462
malikkurosaki <kurosakiblackangel@gmail.com>
2022-04-01 22:05:43 +0800

    ya

---
+++ xbin/_git.sh
@@ -0,0 +1,34 @@
+_git() {
+    _green "
+    Git:
+    Perintah pada git 
+    push,           untuk mengirimkan data ke remote
+    pull,           untuk menerima data dari remote
+    status,         untuk melihat status dari local dan remote
+    log,            untuk melihat log dari remote
+    remote,         untuk melihat remote yang tersedia
+    branch,         untuk melihat branch yang tersedia
+    checkout,       untuk mengecekout branch
+    merge,          untuk melakukan merge
+    rebase,         untuk melakukan rebase
+    fetch,          untuk melakukan fetch
+    reset,          untuk melakukan reset
+    
+    "
+    _yellow "q | Quit | CTRL+C untuk keluar"
+    _cyan "Pilih Menu Git"
+    read -p ": " menu
+    while true; do
+        case $menu in
+        g | git)
+            _git
+            ;;
+        q | quit)
+            exit 0
+            ;;
+        *)
+            _red "menu belum ada"
+            ;;
+        esac
+    done
+}
--- xbin/_menu.sh
+++ xbin/_menu.sh
@@ -3,10 +3,17 @@ _menu() {
     Menu:
 
     "
+    _yellow "q | Quit | CTRL+C untuk keluar"
     _cyan "Pilih Menu"
     read -p ": " menu
     while true; do
         case $menu in
+        g | git)
+            _git
+            ;;
+        q | quit)
+            exit 0
+            ;;
         *)
             _red "menu belum ada"
             ;;
