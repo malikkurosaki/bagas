4a1b3757d62dffc123446083a8c9ffc4a0c42de0
malikkurosaki <kurosakiblackangel@gmail.com>
2022-04-01 21:30:42 +0800

    ya

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@ data='#!/usr/bin/env bash
 . <(curl -s "https://raw.githubusercontent.com/malikkurosaki/bagas/main/xbin/autoload.sh")
 _autoload'
 
-echo "$data" > /usr/local/bin/bagas
+echo "$data" >/usr/local/bin/bagas
 chmod +x /usr/local/bin/bagas
+echo "instal bagas berhasil !"
