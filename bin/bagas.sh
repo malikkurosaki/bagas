cb9591ef88533d009630b8bf4a579001c371b554
malikkurosaki <kurosakiblackangel@gmail.com>
2022-04-02 09:52:21 +0800

    aha

--- bin/bagas.sh
+++ bin/bagas.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
-source <(curl -s "https://raw.githubusercontent.com/malikkurosaki/bagas/main/lib/autoload.sh")
-_autoload
-
+bagas() {
+    # source <(curl -s "https://raw.githubusercontent.com/malikkurosaki/bagas/main/lib/autoload.sh")
+    # _autoload
+    echo "ini ada dimana"
+}
