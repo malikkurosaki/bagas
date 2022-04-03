be1cca483841cce2f294b6f6b64645c77083db4d
malikkurosaki <kurosakiblackangel@gmail.com>
2022-04-04 01:36:10 +0800

    ok

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ const args = process.argv.slice(2);
 const os = require('os');
 const { Exec } = require('./bash');
 const { str } = require('./storage');
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
- ;(async() => {
-     let {stdout} = await  Exec(`git add . && git commit -m "ok" && git push`);
-     console.log(stdout.trim());
- })()
+readline.question('siapa aku', nama => {
+    console.log(`Halo ${nama}`);
+    readline.close();
+})
+
+
+; (async () => {
+    let { stdout } = await Exec(`git add . && git commit -m "ok" && git push`);
+    console.log(stdout.trim());
+})()
 
 // exec(`git add . && git commit -m "ya" && git push`, (err, stdout, stderr) => {
 //     if (err) {
